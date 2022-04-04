54bbb8ab6bdb12fa5bcc5f3eed3092a225e0f548
hikari <hikarimusic2002@gmail.com>
2022-04-04 10:14:34 +0800

    Edit Frame

--- biostats/one_sample_t_test/data.py
+++ biostats/one_sample_t_test/data.py
@@ -17,58 +17,42 @@ class Data(ttk.Frame):
     def setup(self):
 
         # Configure
-        self.columnconfigure(index=2, weight=1)
-        #self.rowconfigure(index=0, weight=1)
-        #self.rowconfigure(index=1, weight=1)
-        self.rowconfigure(index=3, weight=1)
+        self.rowconfigure(index=1, weight=1)
+        self.columnconfigure(index=0, weight=1)
         self.configure(padding=(20,20))
 
-        # Description
-        '''
-        self.description = ttk.Label(
-            self, text="Analyze one dimensional measurement data of one group."
-        )
-        #self.description.config(font="-slant italic")
-        self.description.grid(
-            row=0, column=0, padx=15, pady=(0,10), sticky="nsew"
-        )
-        '''
-
-        # Data Frame
-        '''
-        self.data_frame = ttk.Frame(self)
-        self.data_frame.grid(
+        # Data View
+        self.data_view = ttk.Frame(self)
+        self.data_view.grid(
             row=0, column=0, sticky="nsew"
         )
-        self.data_frame.columnconfigure(index=2, weight=1)
-        '''
-        
+        self.data_view.columnconfigure(index=2, weight=1)
+
         # Button
-        self.edit = ttk.Button(self, text="edit")
-        self.edit.grid(
-            row=0, column=0, padx=(5,0), pady=5, #sticky="nsew"
-        )
-        self.edit.config(command=lambda: self.show(1))
-
-        self.open = ttk.Button(self, text="open")
-        self.open.grid(
-            row=0, column=1, padx=(5,0), pady=5, #sticky="nsew"
+        self.edit_button = ttk.Button(self.data_view, text="Edit")
+        self.edit_button.config(command=lambda: self.show("edit"))
+        self.edit_button.grid(
+            row=0, column=0, padx=5, pady=5, sticky="nsew"
         )
 
-        # Table
-        self.table = ttk.Frame(self)
-        self.table.grid(
-            row=1, column=0, columnspan=5, padx=5, sticky="nsew"
+        self.open_button = ttk.Button(self.data_view, text="Open")
+        self.open_button.config(command=self.open)
+        self.open_button.grid(
+            row=0, column=1, pady=5, sticky="nsew"
         )
-        self.table.columnconfigure(index=0, weight=1)
 
-        self.scrollbar = ttk.Scrollbar(self.table)
-        self.scrollbar.grid(row=0, column=1, sticky="nsew")
+        # Treeview
+        self.scrollbar = ttk.Scrollbar(self.data_view)
+        self.scrollbar.grid(row=1, column=5, padx=(0,5), sticky="nsew")
+
+        self.style = ttk.Style()
+        self.style.configure("Treeview", rowheight=30)
         
         self.tree = ttk.Treeview(
-            self.table, selectmode="none", column=(1), height=20
+            self.data_view, selectmode="none", height=15
         )
-        self.tree.grid(row=0, column=0, sticky="nsew")
+        self.tree.config(column=(1))
+        self.tree.grid(row=1, column=0, columnspan=5, padx=(5,0), sticky="nsew")
 
         self.tree.config(yscrollcommand=self.scrollbar.set)
         self.scrollbar.config(command=self.tree.yview)
@@ -86,7 +70,7 @@ class Data(ttk.Frame):
         
         # Notation
         self.notation = ttk.Checkbutton(
-            self, text="Scientific", style="Switch.TCheckbutton"
+            self.data_view, text="Scientific", style="Switch.TCheckbutton"
         )
         self.notation.grid(
             row=2, column=0, columnspan=2, padx=5, pady=5, sticky="nsew"
@@ -94,26 +78,111 @@ class Data(ttk.Frame):
 
         # Precision
         self.precision_label = ttk.Label(
-            self, text="Precision"
+            self.data_view, text="Precision"
         )
         self.precision_label.grid(
-            row=2, column=3, padx=5, pady=5
+            row=2, column=3, pady=5, sticky="nsew"
         )
 
 
         self.precision = ttk.Spinbox(
-            self, from_=0, to=99, increment=1, width=5
+            self.data_view, from_=0, to=99, increment=1, width=5
         )
         self.precision.insert(0, 0)
         self.precision.grid(
-            row=2, column=4, padx=5, pady=5
+            row=2, column=4, padx=(5,0), pady=5, sticky="nsew"
+        )
+
+        # Data Edit
+        self.data_edit = ttk.Frame(self) 
+        self.data_edit.grid(
+            row=0, column=0, sticky="nsew"
+        )
+        self.data_edit.columnconfigure(index=4, weight=1)
+        self.data_edit.rowconfigure(index=1, weight=1)
+
+        # Control Bar
+        self.row_label = ttk.Label(self.data_edit, text="Row")
+        self.row_label.grid(
+            row=0, column=0, padx=(5,0), pady=5, sticky="nsew"
+        )
+        self.row_spin = ttk.Spinbox(
+            self.data_edit, from_=1, to=999, increment=1, width=6
+        )
+        self.row_spin.grid(
+            row=0, column=1, padx=5, pady=5, sticky="nsew"
+        )
+        self.row_spin.insert(0,10)
+
+        self.column_label = ttk.Label(self.data_edit, text="Column")
+        self.column_label.grid(
+            row=0, column=2, padx=(5,0), pady=5, sticky="nsew"
+        )
+        self.column_spin = ttk.Spinbox(
+            self.data_edit, from_=1, to=999, increment=1, width=6
+        )
+        self.column_spin.grid(
+            row=0, column=3, padx=5, pady=5, sticky="nsew"
+        )
+        self.column_spin.insert(0,10)
+        
+        self.confirm_button = ttk.Button(
+            self.data_edit, text="Confirm", style="Accent.TButton"
+        )
+        self.confirm_button.config(command=self.confirm)
+        self.confirm_button.grid(
+            row=0, column=5, columnspan=2, pady=5, sticky="e"
+        )
+
+        # Table
+        self.table_frame = ttk.Notebook(self.data_edit)
+        self.table_frame.grid(
+            row=1, column=0, columnspan=7, padx=(5,0), sticky="nsew"
+        )
+
+        self.scrollbar2 = ttk.Scrollbar(self.data_edit)
+        self.scrollbar2.grid(
+            row=1, column=7, padx=(0,5), sticky="nsew"
+        )
+
+        # Cell Width
+
+        self.cell_width_label = ttk.Label(
+            self.data_edit, text="Cell Width"
+        )
+        self.cell_width_label.grid(
+            row=2, column=5, padx=5, pady=5, sticky="nsew"
+        )
+
+        self.cell_width = ttk.Spinbox(
+            self.data_edit, from_=1, to=99, increment=1, width=5
+        )
+        self.cell_width.insert(0, 10)
+        self.cell_width.grid(
+            row=2, column=6, padx=(5,0), pady=5, sticky="nsew"
         )
 
         # Export
-        self.export = ttk.Button(self, text="export")
+        self.export = ttk.Button(self, text="Export")
         self.export.grid(
-            row=4, column=3, columnspan=2, sticky="e"
+            row=2, column=0, sticky="e"
         )
 
-    def show(self, frame):
+        # Show
+        self.show("view")
+
+    def show(self, key):
+        
+        if key == "view":
+            frame = self.data_view
+        if key == "edit":
+            frame = self.data_edit
+
+        frame.tkraise()
+
+    def open(self):
         pass
+
+    def confirm(self):
+        
+        self.show("view")
