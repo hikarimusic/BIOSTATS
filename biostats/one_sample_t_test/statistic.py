157bb40652b3f4f04c0087b9ac2d4a7609524846
hikari <hikarimusic2002@gmail.com>
2022-04-08 20:28:55 +0800

    Statistic Table

--- biostats/one_sample_t_test/statistic.py
+++ biostats/one_sample_t_test/statistic.py
@@ -25,7 +25,7 @@ class Statistic(ttk.Frame):
         self.data_view.grid(
             row=0, column=0, sticky="nsew"
         )
-        self.data_view.columnconfigure(index=2, weight=1)
+        self.data_view.columnconfigure(index=4, weight=1)
 
         '''
         # Button
@@ -42,11 +42,36 @@ class Statistic(ttk.Frame):
         )
         '''
 
+        # Control Bar
+        self.percent_label = ttk.Label(self.data_view, text="Percentile(%)")
+        self.percent_label.grid(
+            row=0, column=0, padx=(5,0), pady=5, sticky="nsew"
+        )
+        self.percent_spin = ttk.Spinbox(
+            self.data_view, from_=1, to=99, increment=1, width=5, command=self.tree_update
+        )
+        self.percent_spin.insert(0,90)
+        self.percent_spin.grid(
+            row=0, column=1, padx=5, pady=5, sticky="nsew"
+        )
+
+        self.CI_label = ttk.Label(self.data_view, text="Confidence Level(%)")
+        self.CI_label.grid(
+            row=0, column=2, padx=(5,0), pady=5, sticky="nsew"
+        )
+        self.CI_spin = ttk.Spinbox(
+            self.data_view, from_=1, to=99, increment=1, width=5, command=self.tree_update
+        )
+        self.CI_spin.insert(0,95)
+        self.CI_spin.grid(
+            row=0, column=3, padx=5, pady=5, sticky="nsew"
+        )
+
         # Treeview
         self.scrollbar_1y = ttk.Scrollbar(self.data_view, orient="vertical")
-        self.scrollbar_1y.grid(row=1, column=5, padx=(0,5), sticky="nsew")
+        self.scrollbar_1y.grid(row=1, column=7, padx=(0,5), sticky="nsew")
         self.scrollbar_1x = ttk.Scrollbar(self.data_view, orient="horizontal")
-        self.scrollbar_1x.grid(row=2, column=0, columnspan=5, padx=(5,0), sticky="nsew")
+        self.scrollbar_1x.grid(row=2, column=0, columnspan=7, padx=(5,0), sticky="nsew")
 
         self.style = ttk.Style()
         self.style.configure("Treeview", rowheight=30)
@@ -55,7 +80,7 @@ class Statistic(ttk.Frame):
             self.data_view, selectmode="none", height=10
         )
         self.tree.config(column=(1))
-        self.tree.grid(row=1, column=0, columnspan=5, padx=(5,0), sticky="nsew")
+        self.tree.grid(row=1, column=0, columnspan=7, padx=(5,0), sticky="nsew")
 
         self.tree.config(yscrollcommand=self.scrollbar_1y.set)
         self.scrollbar_1y.config(command=self.tree.yview)
@@ -77,7 +102,7 @@ class Statistic(ttk.Frame):
             variable=self.scientific, command=self.tree_update
         )
         self.notation.grid(
-            row=3, column=0, columnspan=2, padx=5, pady=5, sticky="nsew"
+            row=3, column=0, columnspan=4, padx=5, pady=5, sticky="nsew"
         )
 
         # Precision
@@ -85,7 +110,7 @@ class Statistic(ttk.Frame):
             self.data_view, text="Precision"
         )
         self.precision_label.grid(
-            row=3, column=3, pady=5, sticky="nsew"
+            row=3, column=5, pady=5, sticky="nsew"
         )
 
 
@@ -94,7 +119,7 @@ class Statistic(ttk.Frame):
         )
         self.precision.insert(0,1)
         self.precision.grid(
-            row=3, column=4, padx=(5,0), pady=5, sticky="nsew"
+            row=3, column=6, padx=(5,0), pady=5, sticky="nsew"
         )
 
     def tree_update(self):
@@ -102,8 +127,8 @@ class Statistic(ttk.Frame):
             self.tree.delete(item)
         self.tree.config(column=())
 
-        geometry = self.winfo_toplevel().geometry()
-        self.winfo_toplevel().geometry(geometry)
+        #geometry = self.winfo_toplevel().geometry()
+        #self.winfo_toplevel().geometry(geometry)
 
         column = len(self.model.group)
         try:
@@ -114,7 +139,8 @@ class Statistic(ttk.Frame):
         notation = self.scientific.get()
 
         self.tree.config(column=tuple(range(1,column+1)))
-        self.tree.column("#0", anchor="center", minwidth=180, stretch="yes")
+        #self.tree.column("#0", anchor="center", minwidth=200)
+        self.tree.column("#0", anchor="center", minwidth=200, width=200, stretch="yes")
         self.tree.heading("#0", text="Statistic", anchor="center")
 
         for i in range(column):
@@ -172,7 +198,7 @@ class Statistic(ttk.Frame):
         # Standard Deviation
         value = []
         for i in range(column):
-            if self.model.std[i] == "N.A.":
+            if self.model.std[i] == "-":
                 temp = "-"
             else:
                 if notation == 1:
@@ -189,7 +215,7 @@ class Statistic(ttk.Frame):
         # Variance
         value = []
         for i in range(column):
-            if self.model.var[i] == "N.A.":
+            if self.model.var[i] == "-":
                 temp = "-"
             else:
                 if notation == 1:
@@ -203,18 +229,123 @@ class Statistic(ttk.Frame):
         )
         cnt += 1
 
-        # Range
+        # Standard Error
         value = []
         for i in range(column):
-            if notation == 1:
-                temp = format(round(self.model.range[i],precision), '.{}E'.format(precision))
+            if self.model.sem[i] == "-":
+                temp = "-"
             else:
-                temp = format(round(self.model.range[i],precision), '.{}f'.format(precision))
+                if notation == 1:
+                    temp = format(round(self.model.sem[i],precision), '.{}E'.format(precision))
+                else:
+                    temp = format(round(self.model.sem[i],precision), '.{}f'.format(precision))
             value.append(temp)
             width[i] = max(width[i],len(temp)*10)
         self.tree.insert(
-            parent="", index="end", iid=cnt, text="Range", values=tuple(value)
+            parent="", index="end", iid=cnt, text="Standard Error", values=tuple(value)
         )
+        cnt += 1
+
+        # Confidence Interval
+        try:
+            level = int(float(self.CI_spin.get()))
+        except:
+            level = 95
+
+        self.model.CI_cal(level/100)
+
+        # Two Tailed
+        value = []
+        CI_width = []
+        for i in range(column):
+            if self.model.CI_two[i] == "-":
+                temp = "-"
+            else:
+                if notation == 1:
+                    CI1 = format(round(self.model.CI_two[i][0],precision), '.{}E'.format(precision))
+                    CI2 = format(round(self.model.CI_two[i][1],precision), '.{}E'.format(precision))
+                    temp = "{}~{}".format(CI1, CI2)
+                else:
+                    CI1 = format(round(self.model.CI_two[i][0],precision), '.{}f'.format(precision))
+                    CI2 = format(round(self.model.CI_two[i][1],precision), '.{}f'.format(precision))
+                    temp = "{}~{}".format(CI1, CI2)
+            value.append(temp)
+            CI_width.append(len(temp))
+            width[i] = max(width[i],len(temp)*10)
+        self.tree.insert(
+            parent="", index="end", iid=cnt, text="{}% Confidence Interval".format(level), values=tuple(value)
+        )
+        #self.tree.item(cnt, open=True)
+        CI_iid = cnt
+        cnt += 1
+
+        self.tree.insert(
+            parent=CI_iid, index="end", iid=cnt, text="Two Tailed".format(level), values=tuple(value)
+        )
+        cnt += 1
+
+        # One Tailed
+        value = []
+        for i in range(column):
+            if self.model.CI_one_1[i] == "-":
+                temp = "-"
+            else:
+                if notation == 1:
+                    CI = format(round(self.model.CI_one_1[i],precision), '.{}E'.format(precision))
+                    s = "{}~".format(CI)
+                    temp = '{0: <{l}}'.format(s, l=CI_width[i]+1)
+                else:
+                    CI = format(round(self.model.CI_one_1[i],precision), '.{}f'.format(precision))
+                    s = "{}~".format(CI)
+                    temp = '{0: <{l}}'.format(s, l=CI_width[i]+1)
+            value.append(temp)
+            width[i] = max(width[i],len(temp)*10)
+        self.tree.insert(
+            parent=CI_iid, index="end", iid=cnt, text="One Tailed".format(level), values=tuple(value)
+        )
+        cnt += 1
+
+        value = []
+        for i in range(column):
+            if self.model.CI_one_2[i] == "-":
+                temp = "-"
+            else:
+                if notation == 1:
+                    CI = format(round(self.model.CI_one_2[i],precision), '.{}E'.format(precision))
+                    s = "~{}".format(CI)
+                    temp = '{0: >{l}}'.format(s, l=CI_width[i]+1)
+                else:
+                    CI = format(round(self.model.CI_one_2[i],precision), '.{}f'.format(precision))
+                    s = "~{}".format(CI)
+                    temp = '{0: >{l}}'.format(s, l=CI_width[i]+1)
+            value.append(temp)
+            width[i] = max(width[i],len(temp)*10)
+        self.tree.insert(
+            parent=CI_iid, index="end", iid=cnt, text="One Tailed".format(level), values=tuple(value)
+        )
+        cnt += 1
+
+        # Percentile
+        try:
+            percent = int(float(self.percent_spin.get()))
+        except:
+            percent = 95
+
+        self.model.percent_cal(percent)
+
+        value = []
+        for i in range(column):
+            if notation == 1:
+                temp = format(round(self.model.percent[i],precision), '.{}E'.format(precision))
+            else:
+                temp = format(round(self.model.percent[i],precision), '.{}f'.format(precision))
+            value.append(temp)
+            width[i] = max(width[i],len(temp)*10)
+        self.tree.insert(
+            parent="", index="end", iid=cnt, text="{}th Percentile".format(percent), values=tuple(value)
+        )
+        #self.tree.item(cnt, open=True)
+        per_iid = cnt
         cnt += 1
 
         # Minimum
@@ -227,7 +358,49 @@ class Statistic(ttk.Frame):
             value.append(temp)
             width[i] = max(width[i],len(temp)*10)
         self.tree.insert(
-            parent="", index="end", iid=cnt, text="Minimum", values=tuple(value)
+            parent=per_iid, index="end", iid=cnt, text="Minimum", values=tuple(value)
+        )
+        cnt += 1
+
+        # 1st Quartile
+        value = []
+        for i in range(column):
+            if notation == 1:
+                temp = format(round(self.model.per_25[i],precision), '.{}E'.format(precision))
+            else:
+                temp = format(round(self.model.per_25[i],precision), '.{}f'.format(precision))
+            value.append(temp)
+            width[i] = max(width[i],len(temp)*10)
+        self.tree.insert(
+            parent=per_iid, index="end", iid=cnt, text="1st Quartile", values=tuple(value)
+        )
+        cnt += 1
+
+        # 2nd Quartile
+        value = []
+        for i in range(column):
+            if notation == 1:
+                temp = format(round(self.model.per_50[i],precision), '.{}E'.format(precision))
+            else:
+                temp = format(round(self.model.per_50[i],precision), '.{}f'.format(precision))
+            value.append(temp)
+            width[i] = max(width[i],len(temp)*10)
+        self.tree.insert(
+            parent=per_iid, index="end", iid=cnt, text="2nd Quartile", values=tuple(value)
+        )
+        cnt += 1
+
+        # 3rd Quartile
+        value = []
+        for i in range(column):
+            if notation == 1:
+                temp = format(round(self.model.per_75[i],precision), '.{}E'.format(precision))
+            else:
+                temp = format(round(self.model.per_75[i],precision), '.{}f'.format(precision))
+            value.append(temp)
+            width[i] = max(width[i],len(temp)*10)
+        self.tree.insert(
+            parent=per_iid, index="end", iid=cnt, text="3rd Quartile", values=tuple(value)
         )
         cnt += 1
 
@@ -241,10 +414,30 @@ class Statistic(ttk.Frame):
             value.append(temp)
             width[i] = max(width[i],len(temp)*10)
         self.tree.insert(
-            parent="", index="end", iid=cnt, text="Maximum", values=tuple(value)
+            parent=per_iid, index="end", iid=cnt, text="Maximum", values=tuple(value)
         )
         cnt += 1
 
 
+        '''
+        # Range
+        value = []
+        for i in range(column):
+            if notation == 1:
+                temp = format(round(self.model.range[i],precision), '.{}E'.format(precision))
+            else:
+                temp = format(round(self.model.range[i],precision), '.{}f'.format(precision))
+            value.append(temp)
+            width[i] = max(width[i],len(temp)*10)
+        self.tree.insert(
+            parent="", index="end", iid=cnt, text="Range", values=tuple(value)
+        )
+        cnt += 1
+        '''
+
+
+        # Resize Column
         for i in range(column):
             self.tree.column(i+1, minwidth=width[i])
+
+
